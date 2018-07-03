758da57c459bfad33331158fb10b123461f7a648
Sabhanayagam Baskaran <ctsuser1@MACLTUS54429>
2018-07-03 13:47:33 -0500

    Configure AWS Amplify

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+  s3: {
+    REGION: "us-west-2",
+    BUCKET: "lesson-notes-app-uploads"
+  },
+  apiGateway: {
+    REGION: "us-west-2",
+    URL: "https://q5rqvabqq0.execute-api.us-west-2.amazonaws.com/prod"
+  },
+  cognito: {
+    REGION: "us-west-2",
+    USER_POOL_ID: "us-west-2_rTygPoIaQ",
+    APP_CLIENT_ID: "1d29vfst9bgk0jeqeknet5qopr",
+    IDENTITY_POOL_ID: "us-west-2:0d551d69-38a4-4343-87fe-4256a0dfdc78"
+  }
+};
