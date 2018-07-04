59c97f6e89ce12456e0efd54e6bad5549205089b
Sabhanayagam Baskaran <ctsuser1@MACLTUS54429>
2018-07-04 13:25:14 -0500

    Add the Create Note Page

--- src/config.js
+++ src/config.js
@@ -12,5 +12,19 @@ export default {
     USER_POOL_ID: "us-west-2_rTygPoIaQ",
     APP_CLIENT_ID: "1d29vfst9bgk0jeqeknet5qopr",
     IDENTITY_POOL_ID: "us-west-2:0d551d69-38a4-4343-87fe-4256a0dfdc78"
-  }
+  },
+  MAX_ATTACHMENT_SIZE: 5000000,
 };
+
+// npx aws-api-gateway-cli-test \
+// --username='sabha.b@gmail.com' \
+// --password='Pass4321!' \
+// --user-pool-id='us-west-2_rTygPoIaQ' \
+// --app-client-id='1d29vfst9bgk0jeqeknet5qopr' \
+// --cognito-region='us-west-2' \
+// --identity-pool-id='us-west-2:0d551d69-38a4-4343-87fe-4256a0dfdc78' \
+// --invoke-url='https://q5rqvabqq0.execute-api.us-west-2.amazonaws.com/prod' \
+// --api-gateway-region='us-west-2' \
+// --path-template='/notes' \
+// --method='POST' \
+// --body "{\"content\":\"hello world\",\"attachment\":\"hello.jpg\"}"
---
+++ src/containers/pages/index.js
@@ -0,0 +1,6 @@
+import { H1 } from 'atomic-styled-components';
+
+export default () => 
+<div>
+    <H1>Welcome to Home Page</H1>
+</div>
